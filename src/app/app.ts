38169e1cd1902a96f93d4dd29f6fc8e64e51ee0a
bandr3343 <31930728+bandr3343@users.noreply.github.com>
2023-07-12 09:08:08 +0300

    Indigo.Design Figma Plugin (Community) initial version

---
+++ src/app/app.ts
@@ -0,0 +1,29 @@
+import { html, css, LitElement } from 'lit';
+import { customElement } from 'lit/decorators.js';
+import { Router } from '@vaadin/router';
+import { routes } from './app-routing.js';
+
+@customElement('app-root')
+export default class App extends LitElement {
+  static styles = css`
+    :host {
+      height: 100%;
+      display: flex;
+      justify-content: flex-start;
+      align-items: stretch;
+      align-content: flex-start;
+    }
+  `;
+
+  render() {
+    return html`
+      <link rel='stylesheet' href='../../ig-theme.css'>
+    `;
+  }
+
+  firstUpdated() {
+    const outlet = this.shadowRoot?.querySelector('router-outlet');
+    const router = new Router(outlet);
+    router.setRoutes(routes);
+  }
+}
